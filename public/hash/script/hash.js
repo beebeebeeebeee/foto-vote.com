fa8bd130b270655b224bd9b1fc424cc0de1439f3
beebeebeeebeee <bee.1845698825@gmail.com>
2021-03-27 20:12:46 +0800

    ui fix on desktop view

--- public/hash/script/hash.js
+++ public/hash/script/hash.js
@@ -14,14 +14,20 @@ document.addEventListener("DOMContentLoaded", async () => {
   document.querySelector("#name").innerHTML = data.name;
 
   temp1 = "";
-  data.images.forEach((element, index) => {
+  await asyncForEach(data.images, async (element, index) => {
     vote_result.push({ element: element, vote: null, comment: null });
+    img_direction = `<img src='./images/${element}' style="width: 100%;">`;
+    const dimensions = await imageDimensions(`./images/${element}`);
+    if (dimensions.height > dimensions.width) {
+      img_direction = `<div class="vote-img" style="background-image: url('./images/${element}');"></div>`;
+    }
+    console.log(img_direction);
     temp1 += `
         <div class="card" style="margin-bottom: 10px">
             <h5 class="card-header" style="font-size: 10px;">${element.slice(
               element.indexOf("-") + 1
             )}</h5>
-            <img src="./images/${element}" style="width: 100%"></img>
+            ${img_direction}
             <div class="card-body" style="padding: 0.5rem;">
                 <div class="d-flex justify-content-center">
                   <button onclick="_click(this.id)" id="${index}_like" class="btn btn-success vote-btn">LIKE</button>
@@ -81,4 +87,29 @@ function _submit_vote() {
   }).then((res) => {
     window.location = "./result.html";
   });
+}
+
+// helper to get dimensions of an image
+const imageDimensions = (file) =>
+  new Promise((resolve, reject) => {
+    const img = new Image();
+
+    // the following handler will fire after the successful loading of the image
+    img.onload = () => {
+      const { naturalWidth: width, naturalHeight: height } = img;
+      resolve({ width, height });
+    };
+
+    // and this handler will fire if there was an error with the image (like if it's not really an image or a corrupted one)
+    img.onerror = () => {
+      reject("There was some problem with the image.");
+    };
+
+    img.src = file;
+  });
+
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
 }
